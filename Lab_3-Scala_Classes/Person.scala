2e8ae61ea8c1ba4ccea06f09a79d28deb2b1ec39
Ryan Malani <ryanmalani24@gmail.com>
2023-10-15 21:43:46 -0400

    fixed override vals

--- Lab_3-Scala_Classes/Person.scala
+++ Lab_3-Scala_Classes/Person.scala
@@ -1,3 +1,3 @@
 class Person(val name: String, val email: String)
 
-class Employee(name: String, email: String, val officeNumber: String, val salary: Double) extends Person(name, email)
+class Employee(override val name: String, override val email: String, val officeNumber: String, val salary: Double) extends Person(name, email)
